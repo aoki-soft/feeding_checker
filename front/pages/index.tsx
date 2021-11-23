312846fb6982cd37f0cb09f5b533fb749c70c94b
blz <blz.soft@outlook.jp>
2021-11-23 11:46:50 +0900

    最低限必要なuiボタンを作成した

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -13,7 +13,42 @@ const Home: NextPage = () => {
       />
     </Head>
     <MiniDrawer>
-      <div>メインの部分です</div>
+      <div>12月31日 日曜日</div>
+      
+      <div>
+        <div>
+          えさをあげる人
+        </div>
+        <div>
+          <button>ぷっちょ</button>
+          <button>次男</button>
+          <button>末っ子</button>
+          <button>母</button>
+          <button>父</button>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          えさを与えたら押してください
+        </div>
+        <div>
+          <div>
+            <div>
+              犬1号
+            </div>
+            <button>午前</button>
+            <button>午後</button>
+          </div>
+          <div>
+            <div>
+              犬2号
+            </div>
+            <button>午前</button>
+            <button>午後</button>
+          </div>
+        </div>
+      </div>
     </MiniDrawer>
   </>)
 }
