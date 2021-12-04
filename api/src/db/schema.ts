0eb1e93ea4cd610aff2737b3a3cca3d67e4abbf7
blz <blz.soft@outlook.jp>
2021-12-04 12:23:29 +0900

    えさやり計画のスキーマを追加した

--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -5,26 +5,37 @@ const typeDefs = gql`
     type Pet {
         id: ID! @id @readonly
         name: String!
-        createAt: DateTime @timestamp(operations: [CREATE]) @readonly
+        createAt: DateTime! @timestamp(operations: [CREATE]) @readonly
         updateAt: DateTime @timestamp(operations: [UPDATE]) @readonly
         eating: [Feeding] @relationship(type: "EATE", direction: IN)
     }
     # えさやり
     type Feeding {
         id: ID! @id @readonly
-        createAt: DateTime @timestamp(operations: [CREATE]) @readonly
+        createAt: DateTime! @timestamp(operations: [CREATE]) @readonly
         updateAt: DateTime @timestamp(operations: [UPDATE]) @readonly
-        giver: User @relationship(type: "GIVE", direction: IN)
-        eater: Pet @relationship(type: "EATE", direction: OUT)
-        am_pm: String
+        giver: User! @relationship(type: "GIVE", direction: IN)
+        eater: Pet! @relationship(type: "EATE", direction: OUT)
+        am_pm: String!
     }
     # ユーザー
     type User {
         id: ID! @id @readonly
         name: String!
-        createAt: DateTime @timestamp(operations: [CREATE]) @readonly
+        createAt: DateTime! @timestamp(operations: [CREATE]) @readonly
         updateAt: DateTime @timestamp(operations: [UPDATE]) @readonly
         feeding: [Feeding] @relationship(type: "GIVE", direction: OUT)
+    }
+    # えさやり予定
+    type FeedingSchedule {
+        id: ID! @id @readonly
+        feedingDate: DateTime!
+        am_pm: String!
+        createAt: DateTime! @timestamp(operations: [CREATE]) @readonly
+        updateAt: DateTime @timestamp(operations: [UPDATE]) @readonly
+        giver: User! @relationship(type: "GIVE", direction: IN)
+        eater: Pet! @relationship(type: "EATE", direction: OUT)
+        feeding: Feeding
     }
 `
 
